395436258ac621ad50ab16452a2f70e88de40cd5
Anthony Beaudoin <47396746+Scroran@users.noreply.github.com>
2022-05-16 22:21:54 -0400

    Hangar open on undocking (#2)
    * ss
    
    * r
    
    * Completion of script. Now to test
    
    * Change keyword for hangar
    
    * Working script

--- hangarOpenOnUndocking/Program.cs
+++ hangarOpenOnUndocking/Program.cs
@@ -22,51 +22,94 @@ namespace IngameScript
     partial class Program : MyGridProgram
     {
         bool prevIsDocked, isDocked;
-        List<IMyPistonBase> hangarDoorPistons;
-        IMyShipConnector shipConnector;
+        List<IMyPistonBase> hangarDoorPistons = new List<IMyPistonBase>();
+        List<IMyShipConnector> shipHangarConnectors = new List<IMyShipConnector>();
+        List<bool> prevConnectorState = new List<bool>();
 
+
+        //On launch
         public Program()
         {
             List<IMyTerminalBlock> connectorList = new List<IMyTerminalBlock>();
             GridTerminalSystem.GetBlocksOfType<IMyShipConnector>(connectorList);
+            string hangarKeyword = "Hangar";
+            if (connectorList.Count == 0) throw new System.ArgumentException("No Connector detected at all...", "original"); ;
 
-            //This is bad
-            shipConnector = connectorList[0] as IMyShipConnector;
+            connectorList = FilterBySubstr(connectorList, hangarKeyword);
+            if (connectorList.Count == 0) throw new System.ArgumentException("No Connector with tag *Hangar*", "original"); ;
 
-            //TODO optimal
-            //shipConnector = GridTerminalSystem.GetBlockWithName("*Hangar Connector*");
-            //foreach (IMyShipConnector connector in connectorList){
+            foreach (IMyShipConnector connector in connectorList)
+            {
+                shipHangarConnectors.Add(connector);
+                prevConnectorState.Add(connector.Status.Equals(MyShipConnectorStatus.Connected));
+            }
+            if (shipHangarConnectors.Count != 0)
+                prevIsDocked = isDocked = shipHangarConnectors[0].Status.Equals(MyShipConnectorStatus.Connected);
+            else prevIsDocked = isDocked = false;
+            List<IMyTerminalBlock> pistonList = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyPistonBase>(pistonList);
 
-            //}
+            pistonList = FilterBySubstr(pistonList, hangarKeyword);
 
-            prevIsDocked = isDocked = shipConnector.Status.Equals(MyShipConnectorStatus.Connected);
-            hangarDoorPistons = GridTerminalSystem.GetBlockGroupWithName("*Hangar Pistons*") as List<IMyPistonBase>;
-            //GridTerminalSystem.SearchBlocksOfName("*Hangar Pistons*", hangarDoorPistons);
+            foreach (IMyPistonBase piston in pistonList)
+            {
+                hangarDoorPistons.Add(piston);
+            }
+
             //TODO test if best to auto-update
             //Runtime.UpdateFrequency = UpdateFrequency.Update100;
 
         }
-
+ 
         public void Main(string argument, UpdateType updateSource)
         {
-            if (prevIsDocked)
-            {
-                if (!isDocked)
+            if (IsAnyShipUndocking() && hangarDoorPistons.Count != 0)
+            { 
+                switch (hangarDoorPistons[0].Status)
                 {
-                    switch (hangarDoorPistons[0].Status)
-                    {
-                        case PistonStatus.Extended:
-                        case PistonStatus.Extending:
-                            OpenHangarDoor();
-                            break;
+                    case PistonStatus.Extended:
+                    case PistonStatus.Extending:
+                        OpenHangarDoor();
+                        break;
 
-                        default:
-                            break;
+                    default:
+                        break;
                    
-                    }
                 }
             }
             prevIsDocked = isDocked;
+        }
+        
+        //@Post : update prevConnectorState
+        private bool IsAnyShipUndocking()
+        {
+            bool AShipIsUndocking = false;
+            bool checkedConnectorConnected = false;
+            for (int i = 0; i< shipHangarConnectors.Count; i++)
+            {
+                checkedConnectorConnected = shipHangarConnectors[i].Status.Equals(MyShipConnectorStatus.Connected);
+                if (prevConnectorState[i] != checkedConnectorConnected && !checkedConnectorConnected)
+                {
+                    AShipIsUndocking = true;
+                }
+                prevConnectorState[i] = checkedConnectorConnected;
+            }
+            return AShipIsUndocking;
+        }
+
+        private List<IMyTerminalBlock> FilterBySubstr(List<IMyTerminalBlock> blockList, string subs)
+        {
+
+            List<IMyTerminalBlock> filteredList = new List<IMyTerminalBlock>();
+            foreach (IMyTerminalBlock block in blockList)
+            {
+                if (block.DisplayNameText.Contains(subs))
+                {
+                    filteredList.Add(block);
+
+                }
+            }
+            return filteredList;
         }
 
         private void OpenHangarDoor()
